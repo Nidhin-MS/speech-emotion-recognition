c6eda17e3b6062c84615658fb480daa6742d4a03
Nidhin MS <nidhinreigns@gmail.com>
2026-02-24 17:01:10 +0530

    feature extraction as same as in the model training

--- app.py
+++ app.py
@@ -6,4 +6,19 @@ import tensorflow as tf
 
 model = tf.keras.models.load_model("SER_model.h5")
 
+emotion_labels = ["happy", "sad","angry"]
 
+def extract_features(file_path):
+    audio, sample_rate = librosa.load(file_path, duration=3, offset=0.5)
+
+    mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+
+    max_len = 130
+
+    if mfcc.shape[1] < max_len:
+        pad_width = max_len - mfcc.shape[1]
+        mfcc = np.pad(mfcc, pad_width=((0,0),(0,pad_width)), mode='constant')
+    else:
+        mfcc = mfcc[:, :max_len]
+
+    return mfcc
