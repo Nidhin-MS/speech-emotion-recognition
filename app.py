88cf5c886a3e2f234aaefb992eefe0a56501855a
Nidhin MS <nidhinreigns@gmail.com>
2026-02-24 16:48:21 +0530

    load trained model to app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Speech Emotion Recognition App")
+import streamlit as st
+import numpy as np
+import librosa
+import tensorflow as tf
+
+
+model = tf.keras.models.load_model("SER_model.h5")
+
+
